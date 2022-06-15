2dab5d3b539d5d658b324d851ddadcbdf4c50b2c
Nick Warren <nickwarren47@gmail.com>
2022-06-14 19:28:11 -0600

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const main = document.getElementsByTagName("main")[0];
+main.remove();
+
+const newHeader = document.createElement("h1");
+// const victory = document.getElementsById("newHeader");
+newHeader.textContent = "Nick is the champion";
+newHeader.id = "victory";
+document.body.append(newHeader);
+
+
+
 // Write your code here!
