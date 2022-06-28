e04f62dd09246742c9e32ec87ed81019dcc4b05f
Nick Warren <nickwarren47@gmail.com>
2022-06-28 10:35:00 -0600

    Done with assignment

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-const main = document.getElementsByTagName("main")[0];
-main.remove();
+// const main = document.getElementsByTagName("main")[0];
+// main.remove();
 
-const newHeader = document.createElement("h1");
-// const victory = document.getElementsById("newHeader");
-newHeader.textContent = "Nick is the champion";
-newHeader.id = "victory";
-document.body.append(newHeader);
+// const newHeader = document.createElement("h1");
+// // const victory = document.getElementsById("newHeader");
+// newHeader.textContent = "Nick is the champion";
+// newHeader.id = "victory";
+// document.body.append(newHeader);
 
 
 
 // Write your code here!
+
+
+
+
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+    // const victory = document.getElementsById("newHeader");
+
+newHeader.id = 'victory';
+newHeader.innerHTML = "Nick is the champion";
+document.body.append(newHeader);
+
+
+
